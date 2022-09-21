bebc2612ccfce0babeb193a37e8fca9bce173b9f
Wandile Mawelela <wandile1997@gmail.com>
2022-09-21 21:46:47 +0200

    Added new uppercase file

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str = ""
+    for ch in str:
+        if 97 <= ord(ch) <= 122:
+            x = ord(ch) - 32
+            y = char(x)
+            str = str + y
