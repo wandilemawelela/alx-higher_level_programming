9d5883537834b57825123b0cd68adc476d197483
Wandile Mawelela <wandile1997@gmail.com>
2022-09-24 00:56:37 +0200

    Fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
+    import sys
 
     if len(sys.argv) == 1:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>", end="\n")
