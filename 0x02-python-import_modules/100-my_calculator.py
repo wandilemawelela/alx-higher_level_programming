e38619b9f76ce8f0530d30528b3f2fe95cb6b9aa
Wandile Mawelela <wandile1997@gmail.com>
2022-09-24 01:14:40 +0200

    Fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,4 +21,4 @@ if __name__ == "__main__":
         elif sys.argv[2] == '*':
             print("{} * {} = {}".format(a, b, mul(a, b)))
         elif sys.argv[2] == '/':
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            print("{} / {} = {}".format(a, b, int(div(a, b))))
