7cadde77972411ccf7d6e2a578261c281f8f2f17
Wandile Mawelela <wandile1997@gmail.com>
2022-09-24 01:30:24 +0200

    Bug Fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
         exit(1)
     else:
         a, b = int(sys.argv[1]), int(sys.argv[3])
-        if sys.argv[2] == '+':
+        if sys.argv[2] == operand[0]:
             print("{} + {} = {}".format(a, b, add(a, b)))
-        elif sys.argv[2] == '-':
+        elif sys.argv[2] == operand[1]:
             print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif sys.argv[2] == '*':
+        elif sys.argv[2] == operand[2]:
             print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif sys.argv[2] == '/':
+        elif sys.argv[2] == operand[3]:
             print("{} / {} = {}".format(a, b, div(a, b)))
