476c577bf2026f26b497f409d3e6066615d65321
Wandile Mawelela <wandile1997@gmail.com>
2022-09-26 23:34:22 +0200

    Fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     for arr in matrix:
         for row in arr:
-            print("{:d} ".format(row), end="")
+            print("{:d}".format(row), end="") \
+                if row == 3 or row == 6 or row == 9 \
+                else print("{:d} ".format(row), end="")
         print("")
+
+
