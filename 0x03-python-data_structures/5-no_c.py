36270ff3b322b7705d24a4e001513ba642c83012
Wandile Mawelela <wandile1997@gmail.com>
2022-09-26 22:55:10 +0200

    Bug fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return my_string.translate({ord("C"): None}) \
-        if "C" in my_string else my_string.translate({ord('c'): None})
+    return my_string.translate({ord('C'): None}) \
+        if 'C' in my_string else my_string.translate({ord('c'): None})
