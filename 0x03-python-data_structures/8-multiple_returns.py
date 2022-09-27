2a8a6c1733528f5e06a19ad8a18ba2c1e7e8f070
Wandile Mawelela <wandile1997@gmail.com>
2022-09-27 12:46:08 +0200

    Added new fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == " ":
-        first = None
-        return first
-    else:
-        length, first = len(sentence), sentence[0]
-        return length, first
+    return (0, None) if sentence == "" else len(sentence), sentence[0]
