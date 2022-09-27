d3e7502855ce1a66d9ffdca5b5337bf07dcc3498
Wandile Mawelela <wandile1997@gmail.com>
2022-09-27 12:31:51 +0200

    Added new multiple return file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length, first = len(sentence), sentence[0]
-    return None if sentence == "" else length, first
+    return None if sentence == " " else length, first
