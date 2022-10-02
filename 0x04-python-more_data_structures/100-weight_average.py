21cfbd1b1152f4ec3049550a0bdbc9cf21e82490
Wandile Mawelela <wandile1997@gmail.com>
2022-10-02 10:43:27 +0200

    [ADD] Weight avr file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    else:
+        sum = 0
+        for sub in my_list:
+            for i in sub :
+                sum = i * i
+        result = sum / len(my_list)
+    return result
