5fa4babab86b0bd4d2425f65ba167bc591a7f34f
Wandile Mawelela <wandile1997@gmail.com>
2022-10-02 19:36:48 +0200

    [FIX] Missing checkers

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
-        return 0
-    else:
-        sum = 0
-        for sub in my_list:
-            for i in sub:
-                sum = i * i
-        result = sum / len(my_list)
-    return result
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+
+    avg = 0
+    size = 0
+    for tup in my_list:
+        avg += (tup[0] * tup[1])
+        size += tup[1]
+    return (avg / size)
