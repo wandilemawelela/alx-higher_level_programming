6ef240bd0c24f4201077fedb9f4a37c6f140aef1
Wandile Mawelela <wandile1997@gmail.com>
2023-10-04 21:34:57 +0200

    [FIX] Print formatting

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end=" ")
         elif num % 3 == 0:
-            print("Fizz")
+            print("Fizz", end=" ")
         elif num % 5 == 0:
-            print("Buzz")
+            print("Buzz", end=" ")
         else:
             print(num, end=" ")
