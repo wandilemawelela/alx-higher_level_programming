46430ceeb3722b46904ef03cd0bca99328f6afeb
Wandile Mawelela <wandile1997@gmail.com>
2023-10-08 15:23:07 +0200

    [ADD] Completed task 6: Write a function that prints a matrix of integers.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for arr in matrix:
+        for row in arr:
+            print("{:d}".format(row), end="") if row % 3 == 0 else print("{:d} ".format(row), end="")
+        print("")
