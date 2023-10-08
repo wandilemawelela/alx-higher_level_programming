3ac0e670318e40ea7a69c20700de2beda038bc50
Wandile Mawelela <wandile1997@gmail.com>
2023-10-08 16:37:10 +0200

    [FIX] Hot fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for arr in matrix:
-        row_str = " ".join(
-            str(row) if row % 3 != 0 else str(row) for row in arr
-        )
-        print(row_str)
+        for row in arr:
+            print("{:d}".format(row), end="") if row % 3 == 0 else \
+                print("{:d} ".format(row), end="")
+        print("")
