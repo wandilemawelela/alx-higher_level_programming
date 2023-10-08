29af89caf0a2b276a63b04f9b99b1f9d2aa4d381
Wandile Mawelela <wandile1997@gmail.com>
2023-10-08 16:58:33 +0200

    [FIX] Handles edge cases and avoids potential issues with mutable default arguments.

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
-def max_integer(my_list=[]):
+def max_integer(my_list=None):
     if my_list is None:
         return None
-    else:
-        my_list.sort()
-        return my_list[-1]
+
+    if len(my_list) == 0:
+        return None
+
+    max_value = my_list[0]
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+
+    return max_value
