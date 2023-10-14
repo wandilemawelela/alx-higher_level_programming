53f7b56b7940fb784fce815a29e9d993c0be0563
Wandile Mawelela <wandile1997@gmail.com>
2023-10-14 16:19:49 +0200

    [ADD] Completed task 10: Write a function in C that checks if a singly linked list has a cycle in it.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: Linked list structure
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ *
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list->next;
+
+	/** Check if list is empty or has one node **/
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+
+
+	/** Move slow pointer one step and fast pointer by two steps **/
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+		{
+			return (1);
+		}
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
