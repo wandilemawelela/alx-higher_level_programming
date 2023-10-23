f281bc7f6620bb5c9fa67f18b286a1196ee7df75
Wandile Mawelela <wandile1997@gmail.com>
2023-10-23 17:13:41 +0200

    [FIX] Logical bug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-
     try:
+        count = 0
         for i in range(x):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end=" ")
                 count += 1
-    except TypeError:
-        pass
-
-    print()
-    return count
+    except TypeError as err:
+        print(err)
+    else:
+        print("")
+        return count
