60b4a0f9a395bb612c22a1f539435e8ad8a4051d
Wandile Mawelela <wandile1997@gmail.com>
2023-10-23 17:16:23 +0200

    [FIX] Refactored to se isinstance function

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,10 +3,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
+
         for i in range(x):
-            if type(my_list[i]) is int:
-                print("{:d}".format(my_list[i]), end=" ")
+            if isinstance(my_list[i], int):
                 count += 1
+                print("{:d}".format(my_list[i]), end="")
     except TypeError as err:
         print(err)
     else:
