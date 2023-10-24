85922fc2dcc4ed52bcb0a62c7d4337f1bc06d3a1
Wandile Mawelela <wandile1997@gmail.com>
2023-10-24 21:05:18 +0200

    [ADD] Completed task 1: Write a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a Square module.
+
+This module provides a private instance attribute size
+"""
+
+
+class Square:
+    """
+    A very simple Square class that provides a size
+    private instance attribute
+    """
+    def __init__(self, size):
+        self.__size = size
