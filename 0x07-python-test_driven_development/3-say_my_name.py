e2c770ab7649b11ddb4655e36023acf097f61aac
Wandile Mawelela <wandile1997@gmail.com>
2023-10-31 21:14:40 +0200

    [FIX] Pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+"""
+This is the Say My Name module
+
+This module has the say_my_name() fuction
+that takes in a first_name and a last_name
+as parameters the prints out "My name is "
+and the values passed to the parameters.
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Prints the value passed in the parameters of
+    the say_my_name fucntion. Otherwise raises a
+    TypeError is any of the parameters is not a string
+    """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     else:
         print("My name is {} {}".format(first_name, last_name))
-
