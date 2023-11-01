bdd01e38bd95c4fb65b94192cc73b456f2a8b570
Wandile Mawelela <wandile1997@gmail.com>
2023-11-01 20:48:27 +0200

    [ADD] Completed task 1: Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This is the Rectangle module.
+
+This module provides a Rectangle class with the attributes
+width and height with default values of 0.
+"""
+
+
+class Rectangle:
+    """The Rectangle class with width and height attributes"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if width < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if height < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
