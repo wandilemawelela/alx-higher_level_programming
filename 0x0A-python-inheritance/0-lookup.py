218a5e067e1a1ede6fd4a28dad9862196583b76d
Wandile Mawelela <wandile1997@gmail.com>
2023-11-06 22:29:04 +0200

    [FIX] Added docstring comments

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines an obj attribute lookup function"""
+
+
 def lookup(obj):
+    """Return a list of an object's available attributes."""
     attributes = []
     methods = []
 
