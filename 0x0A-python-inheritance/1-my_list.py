baafbee3ab7f2fff4812f478eda25933a7cdfda2
Wandile Mawelela <wandile1997@gmail.com>
2023-11-06 22:35:05 +0200

    [FIX] Code refactor

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,5 +4,10 @@
 
 class MyList(list):
     """MyList subclass that prints the list that is sorted."""
+    def __init__(self):
+        """Init object"""
+        super().__init__()
+
     def print_sorted(self):
+        """Print sorted list"""
         print(sorted(self))
