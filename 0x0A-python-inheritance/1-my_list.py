1eb5376e11e1ac2c9e7f1f165f44b32703aaff2e
Wandile Mawelela <wandile1997@gmail.com>
2023-11-06 22:32:48 +0200

    [FIX] Docstring comments

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Defines the class MyList"""
+
+
 class MyList(list):
+    """MyList subclass that prints the list that is sorted."""
     def print_sorted(self):
         print(sorted(self))
