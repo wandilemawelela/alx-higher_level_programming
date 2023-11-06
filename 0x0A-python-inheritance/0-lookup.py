267eb1dc77237a2e8dce48bcd3d63c8b6f40285d
Wandile Mawelela <wandile1997@gmail.com>
2023-11-06 21:36:31 +0200

    [FIX] Refactor

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def lookup(obj):
-    return dir(obj)
+    attributes = []
+    methods = []
+
+    for attr in dir(obj):
+        if not callable(getattr(obj, attr)):
+            attributes.append(attr)
+
+    for attr in dir(obj):
+        if callable(getattr(obj, attr)):
+            methods.append(attr)
+
+    return attributes + methods
