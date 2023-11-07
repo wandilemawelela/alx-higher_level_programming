03fc142138110f2c4b9ed4a659e506fbc0901d8c
Wandile Mawelela <wandile1997@gmail.com>
2023-11-07 20:53:26 +0200

    [FIX] Pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Prints the contents of a text file to stdout"""
     with open(filename, encoding='utf-8') as a_file:
-            print(a_file.read(), end="")
+        print(a_file.read(), end="")
