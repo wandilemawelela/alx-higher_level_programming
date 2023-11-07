c0f8cf59d1ee45c1210050f5d8b13d5f468e6418
Wandile Mawelela <wandile1997@gmail.com>
2023-11-07 21:26:01 +0200

    [ADD] Compelted task 3:Write a function that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import json
+"""Defines a JSON string function"""
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of the my_obj string"""
+    return json.dumps(my_obj)
