3c726b15254ef6c344342ae4cedb559bd0137998
Wandile Mawelela <wandile1997@gmail.com>
2023-11-07 20:51:46 +0200

    [FIX] print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,5 @@
 
 def read_file(filename=""):
     """Prints the contents of a text file to stdout"""
-    line_number = 0
-
     with open(filename, encoding='utf-8') as a_file:
-        for a_line in a_file:
-            line_number += 1
-            print('{}'.format(a_line.strip()))
+            print(a_file.read(), end="")
