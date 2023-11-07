160adb3d686960236e449bacd78a9290a616f0f8
Wandile Mawelela <wandile1997@gmail.com>
2023-11-07 22:35:24 +0200

    [FIX] Pycodestyle

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,6 +2,7 @@
 """Defines a load from json function"""
 import json
 
+
 def load_from_json_file(filename):
     """Creates an Object from a “JSON file” """
     with open(filename, 'r', encoding='utf-8') as file:
