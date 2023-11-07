9d231acdf5686b158134e9d4e93ba8db07d97ec1
Wandile Mawelela <wandile1997@gmail.com>
2023-11-07 22:33:40 +0200

    [ADD] Completed task 6: Write a function that creates an Object from a “JSON file”

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a load from json function"""
+import json
+
+def load_from_json_file(filename):
+    """Creates an Object from a “JSON file” """
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
