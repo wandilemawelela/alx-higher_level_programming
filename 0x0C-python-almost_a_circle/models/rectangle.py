d568b28bd7d47cdd48042a62db06aa48f94f06d6
Wandile Mawelela <wandile1997@gmail.com>
2023-11-11 20:02:54 +0200

    [ADD] Completed task 6: Update the class Rectangle by improving the public method def display(self): to print in stdout the Rectangle instance with the character # by taking care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,10 +98,13 @@ class Rectangle(Base):
 
     def display(self):
         """
-        Display the Rectangle instance with '#' characters.
+        Display the Rectangle instance with '#' characters,
+        considering the offset x and y.
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """"Return value for str()"""
