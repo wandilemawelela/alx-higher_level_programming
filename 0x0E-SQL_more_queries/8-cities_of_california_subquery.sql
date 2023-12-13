6e665d7667ed69a4448c6a2def5cd4711769c0a1
wandilemawelela <wandile1997@gmail.com>
2023-12-13 12:09:05 -0800

    [FIX] Hot fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,11 +2,10 @@
 -- The states table contains only one record where name = California (but the id can be different, as per the example)
 -- Results must be sorted in ascending order by cities.id
 
-USE hbtn_0d_usa;
-
--- List all cities of California without using JOIN
-SELECT cities.*
+SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY cities.id ASC;
-
+WHERE state_id IN (
+	SELECT id
+	FROM states
+        WHERE name = "California")
+ORDER BY id;
