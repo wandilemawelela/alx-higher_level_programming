9e5bcba1fde7eabe55be3ec92b13be8fdff0cffb
Wandile Mawelela <wandile1997@gmail.com>
2024-01-09 22:09:05 +0200

    [FIX] Hot fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    }
-    this.width = w;
-    this.height = h;
-    }
-}
-
-module.exports = Rectangle;
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+};
