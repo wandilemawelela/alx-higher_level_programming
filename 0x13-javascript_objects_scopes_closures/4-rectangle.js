b8a7dd6b6a7ace51122e9e49acbf85239831e821
Wandile Mawelela <wandile1997@gmail.com>
2024-01-09 22:29:49 +0200

    [ADD] Completed task 4: Write a class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  rotate () {
+     [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+};
