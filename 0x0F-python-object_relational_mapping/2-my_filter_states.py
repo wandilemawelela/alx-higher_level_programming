602ef2fbbe16e52056cf97cfda32b626a170b733
wandilemawelela <wandile1997@gmail.com>
2024-01-12 14:38:33 -0800

    [ADD] Completed task 2: Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+def search_states(username, password, database, state_name):
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Use format to create the SQL query with the user input
+    query = "SELECT * FROM states WHERE name = \
+    '{}' ORDER BY states.id ASC".format(state_name)
+
+    # Execute the SQL query
+    cursor.execute(query)
+
+    # Fetch all rows from the result set
+    matching_states = cursor.fetchall()
+
+    # Display the results
+    for state in matching_states:
+        print(state)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> \
+                <database> <state_name>")
+        sys.exit(1)
+
+    # Extract username, password, database name, and state
+    # name from command-line arguments
+    username, password, database, state_name = (
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3],
+            sys.argv[4]
+            )
+
+    # Call the function to search for states with the provided name
+    search_states(username, password, database, state_name)
