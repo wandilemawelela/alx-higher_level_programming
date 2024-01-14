def59937cc0fd8a4b71871af6c9ef82d27816feb
wandilemawelela <wandile1997@gmail.com>
2024-01-14 14:19:43 -0800

    [FIX] Hot fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,44 +1,23 @@
 #!/usr/bin/python3
 """
-Script that prints the first State object from the database hbtn_0e_6_usa.
+Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa.
+Usage: ./9-model_state_filter_a.py <mysql username> /
+                                   <mysql password> /
+                                   <database name>
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
-
-def print_first_state(username, password, database):
-    """Prints the first State object from the specified database."""
-    engine = create_engine(f'mysql+mysqldb://{username}:\
-            {password}@localhost:3306/{database}')
-
-    # Create all tables in the engine
-    Base.metadata.create_all(engine)
-
-    # Create a session to interact with the database
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query for the first State object ordered by id
-    first_state = session.query(State).order_by(State.id).first()
-
-    # Print the result
-    if first_state:
-        print(f"{first_state.id}: {first_state.name}")
-    else:
-        print("Nothing")
-
-    # Close the session
-    session.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python print_first_state.py <username> <password>\
-                <database>")
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    print_first_state(username, password, database)
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
