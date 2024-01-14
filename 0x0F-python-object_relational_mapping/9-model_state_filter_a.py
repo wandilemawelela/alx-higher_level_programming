0781b468bd2e2d7e482f4ae00d14e51ae54bbbf8
wandilemawelela <wandile1997@gmail.com>
2024-01-14 14:11:28 -0800

    [ADD] Completed task 9: Write a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script that prints the first State object from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def print_first_state(username, password, database):
+    """Prints the first State object from the specified database."""
+    engine = create_engine(f'mysql+mysqldb://{username}:\
+            {password}@localhost:3306/{database}')
+
+    # Create all tables in the engine
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query for the first State object ordered by id
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Print the result
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python print_first_state.py <username> <password>\
+                <database>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    print_first_state(username, password, database)
